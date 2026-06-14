05977fe7509462bc52adaabf773660a5a748c27c
sa2000-2 <sato.asuka2000@gmail.com>
2026-06-15 02:53:35 +0900

    docs: RecommendControllerのJavadocを追加、修正
    @ModelAttributeを削除
    Spring Boot の仕様により省略しても機能するため削除しました

--- src/main/java/com/example/extensivereading/controller/RecommendController.java
+++ src/main/java/com/example/extensivereading/controller/RecommendController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +21,10 @@ import com.example.extensivereading.entity.Favorite;
 import com.example.extensivereading.security.UserDetailsImpl;
 import com.example.extensivereading.service.RecommendService;
 
+/**
+ * AIおすすめ本表示機能とお気に入り登録に関するリクエストを制御するControllerクラス。
+ * 画面表示(GET)とAIおすすめリストの取得、お気に入りの登録、変更、削除処理(POST)を管理し適切な画面へ誘導する。
+ */
 @Controller
 @RequestMapping("/recommend")
 public class RecommendController {
@@ -32,14 +35,27 @@ public class RecommendController {
         this.recommendService = recommendService;
     }
 
+    /**
+     * AIおすすめ本表示画面を表示
+     * @param model おすすめのための選択肢データを運ぶためのモデル箱
+     * @return AIおすすめ本表示画面のテンプレート名
+     */
     @GetMapping
     public String showRecommendForm(Model model) {
         model.addAttribute("recommendForm", new RecommendForm());
         return "recommend";
     }
 
+    
+    /**
+     * AIに尋ねる
+     * @param form 質問のための選択肢を運ぶ箱
+     * @param result 入力チェックの結果
+     * @param model おすすめリストやエラーメッセージを運ぶためのモデル箱
+     * @return AIおすすめ本表示画面のテンプレート名
+     */
     @PostMapping("/ask")
-    public String askAI(@Validated @ModelAttribute("recommendForm") RecommendForm form, 
+    public String askAI(@Validated RecommendForm form, 
             BindingResult result, 
             Model model) {
     	if (result.hasErrors()) {
@@ -58,10 +74,19 @@ public class RecommendController {
         return "recommend"; 
     }
 
+    
+    /**
+     * おすすめ本をお気に入り登録する
+     * @param userDetails ログイン中のユーザー情報
+     * @param favoriteForm お気に入り登録する本のデータの箱
+     * @param result 入力チェックの結果
+     * @param redirectAttributes リダイレクトの時にメッセージを運ぶための箱
+     * @return AIおすすめ本表示画面にとぶためのURL、またはお気に入り登録リスト画面にとぶためのURL
+     */
     @PostMapping("/favorite")
     public String addFavorite(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @Validated @ModelAttribute("favoriteForm") FavoriteForm favoriteForm,
+            @Validated FavoriteForm favoriteForm,
             BindingResult result,
             RedirectAttributes redirectAttributes) {
 
@@ -83,6 +108,13 @@ public class RecommendController {
         return "redirect:/recommend/favorites";
     }
 
+    
+    /**
+     * お気に入り登録リスト画面を表示
+     * @param userDetails ログイン中のユーザー情報
+     * @param model お気に入り登録リストをHTMLに運ぶためのモデル箱
+     * @return お気に入り登録リスト画面のテンプレート名
+     */
     @GetMapping("/favorites")
     public String showFavorites(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
         List<Favorite> favoriteList = recommendService.getFavorites(userDetails.getUsername());
@@ -90,6 +122,14 @@ public class RecommendController {
         return "favoriteList";
     }
 
+    
+    /**
+     * お気に入り登録を削除する
+     * @param userDetails ログイン中のユーザー情報
+     * @param id お気に入り登録ID
+     * @param redirectAttributes リダイレクトの時にメッセージを運ぶための箱
+     * @return お気に入り登録リスト画面にとぶためのURL
+     */
     @PostMapping("/favorite/delete/{id}")
     public String deleteFavorite(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                  @PathVariable Integer id,
